5ceef55b538860d73f9681308ac3f1c2d97427d1
Amey Parulekar <amey@wirywolf.com>
2012-01-21 14:40:51 +0530

    Added global symbol lookup, changed default binding from C-] to C-\. Fixed Mac keybindings.

--- README.rst
+++ README.rst
@@ -14,10 +14,11 @@ Installation
 
 1. Install Cscope (a Windows port can be found `here`_)
 2. Generate a cscope database (cscope.out) in the root directory of your project
-3. Check out the repo under``Packages`` and restart Sublime Text.
+3. Check out the repo under``Packages`` or install via `Package Control` and restart Sublime Text.
 
 .. _here: http://code.google.com/p/cscope-win32
+.. _Package Control: http://wbond.net/sublime_packages/package_control
 
 Overview
 ========
-Only jump-to-definition works at the moment. Default keybinding is Ctrl+]. Rewinding to the previous position in the stack with Ctrl+t hasn't been added yet. The plugin will recursively search for the cscope database in parent directories of the currently open file until it either finds the database or reaches the root directory.
+Only global symbol lookup and jump to function definition work at the moment. Default keybinding is Ctrl/Super+\. Rewinding to the previous position in the stack with Ctrl+t hasn't been added yet. The plugin will recursively search for the cscope database in parent directories of the currently open file until it either finds the database or reaches the root directory.
--- cscope.py
+++ cscope.py
@@ -3,25 +3,17 @@ import os
 import re
 import subprocess
 
-class JumpToDefinitionCommand(sublime_plugin.TextCommand):
+class CscopeCommand(sublime_plugin.TextCommand):
     def __init__(self, view):
         self.view = view
         self.database = None
         #self.panel  = self.view.window().get_output_panel("cscope")
-
-    def run(self, edit):
+    
+    def run(self, edit, mode):
         # self.word_separators = self.view.settings().get('word_separators')
         # print self.view.sel()
         # self.view.insert(edit, 0, "Hello, World!")
-        if self.database == None:
-            self.find_database()
-
-        newline = ''
-        if sublime.platform() == "windows":
-            newline = '\r\n'
-        else:
-            newline = '\n'
-
+        print mode
         one = self.view.sel()[0].a
         two = self.view.sel()[0].b
         self.view.sel().add(sublime.Region(one,
@@ -29,39 +21,7 @@ class JumpToDefinitionCommand(sublime_plugin.TextCommand):
         for sel in self.view.sel():
             word = self.view.substr(self.view.word(sel))
             #print "Word: ", word
-            # 0 ==> C symbol
-            # 1 ==> function definition
-            # 2 ==> functions called by this function
-            # 3 ==> functions calling this function
-            # 4 ==> text string
-            # 5 ==> egrep pattern
-            # 6 ==> files
-            cscope_arg_list = ['cscope', '-dL', '-f', self.database, '-1' + word]
-            popen_arg_list = {
-                              "shell": False,
-                              "stdout": subprocess.PIPE,
-                              "stderr": subprocess.PIPE
-                              }
-            if (sublime.platform() == "windows"):
-                popen_arg_list["creationflags"] = 0x08000000
- 
-            output = self.proc.communicate()[0].split(newline)
-            print output
-            self.matches = []
-            for i in output:
-                match = re.match('(\S+?)\s+?' + word + '\s+(\d+)\s+(.+)', i)
-                if match != None:
-                    self.matches.append({
-                                    "file": match.group(1),
-                                    "line": match.group(2),
-                                    "instance": match.group(3)
-                                    })
-                    #print "File ", match.group(1), ", Line ", match.group(2), ", Instance ", match.group(3)
- 
-            #self.view.window().run_command("show_overlay", {"overlay": "goto", "text": "@"})
-            options = []
-            for match in self.matches:
-                options.append("%(file)s:%(line)s - %(instance)s" % match)
+            options = self.run_cscope(mode, word)
             self.view.window().show_quick_panel(options, self.on_done)
             #self.view.window().run_command("show_panel", {"panel": "output." + "cscope"})
     
@@ -86,3 +46,70 @@ class JumpToDefinitionCommand(sublime_plugin.TextCommand):
         
         if self.database == None:
             sublime.status_message("Could not find cscope database: cscope.out")
+    
+    def run_cscope(self, mode, word):
+        # 0 ==> C symbol
+        # 1 ==> function definition
+        # 2 ==> functions called by this function
+        # 3 ==> functions calling this function
+        # 4 ==> text string
+        # 5 ==> egrep pattern
+        # 6 ==> files
+
+        if self.database == None:
+            self.find_database()
+
+        newline = ''
+        if sublime.platform() == "windows":
+            newline = '\r\n'
+        else:
+            newline = '\n'
+
+        cscope_arg_list = ['cscope', '-dL', '-f', self.database, '-' + str(mode) + word]
+        popen_arg_list = {
+                          "shell": False,
+                          "stdout": subprocess.PIPE,
+                          "stderr": subprocess.PIPE
+                          }
+        if (sublime.platform() == "windows"):
+            popen_arg_list["creationflags"] = 0x08000000
+
+        proc = subprocess.Popen(cscope_arg_list, **popen_arg_list)
+        output = proc.communicate()[0].split(newline)
+        #print output
+        matches = []
+        for i in output:
+            match = self.match_output_line(i, mode)
+            if match != None:
+                matches.append(match)
+                #print "File ", match.group(1), ", Line ", match.group(2), ", Instance ", match.group(3)
+
+        #self.view.window().run_command("show_overlay", {"overlay": "goto", "text": "@"})
+        options = []
+        for match in matches:
+            options.append("%(file)s:%(line)s - %(instance)s" % match)
+        
+        return options
+    
+    def match_output_line(self, line, mode):
+        match = None
+
+        if mode == 0:
+            match = re.match('(\S+?)\s+?(<global>)?\s+(\d+)\s+(.+)', line)
+        elif mode == 1:
+            match = re.match('(\S+?)\s+?\S+\s+(\d+)\s+(.+)', line)
+ 
+        if match != None:
+            if match.lastindex == 4:
+                return {
+                        "file": match.group(1),
+                        "line": match.group(3),
+                        "instance": match.group(4)
+                        }
+            else:
+                return {
+                        "file": match.group(1),
+                        "line": match.group(2),
+                        "instance": match.group(3)
+                        }
+        return None
